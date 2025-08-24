5f90d2355b75f05e9bfdfbd09170b9d746f37ad0
JhonECG <jhonchilo184@gmail.com>
2025-08-23 23:44:08 -0500

    Actulización de plantilla

--- main.cpp
+++ main.cpp
@@ -4,20 +4,68 @@
 #include <string>
 using namespace std;
 
-struct Record {
+class Matricula {
     string codigo;
     int ciclo;
     float mensualidad;
     string observaciones;
 
-    string pack() const {
-        // TODO: serializar campos a string binario
-        return "";
+public:
+    void setCodigo(const string& c) {
+        codigo = c;
+    }
+    void setCiclo(int c) {
+        ciclo = c;
+    }
+    void setMensualidad(float m) {
+        mensualidad = m;
+    }
+    void setObservaciones(const string& o) {
+        observaciones = o;
+    }
+    string getCodigo() const {
+        return codigo;
+    }
+    int getCiclo() const {
+        return ciclo;
+    }
+    float getMensualidad() const {
+        return mensualidad;
+    }
+    string getObservaciones() const {
+        return observaciones;
     }
 
-    static Record unpack(const string& data) {
-        // TODO: deserializar desde string binario
-        return Record();
+    string pack() const {
+        string data;
+        int len_codigo = codigo.size();
+        int len_obs = observaciones.size();
+        data.append(reinterpret_cast<const char*>(&len_codigo), sizeof(int));
+        data.append(codigo);
+        data.append(reinterpret_cast<const char*>(&ciclo), sizeof(int));
+        data.append(reinterpret_cast<const char*>(&mensualidad), sizeof(float));
+        data.append(reinterpret_cast<const char*>(&len_obs), sizeof(int));
+        data.append(observaciones);
+        return data;
+    }
+
+    static Matricula unpack(const string& data) {
+        Matricula m;
+        size_t pos = 0;
+        int len_codigo = 0;
+        memcpy(&len_codigo, data.data() + pos, sizeof(int));
+        pos += sizeof(int);
+        m.codigo = data.substr(pos, len_codigo);
+        pos += len_codigo;
+        memcpy(&m.ciclo, data.data() + pos, sizeof(int));
+        pos += sizeof(int);
+        memcpy(&m.mensualidad, data.data() + pos, sizeof(float));
+        pos += sizeof(float);
+        int len_obs = 0;
+        memcpy(&len_obs, data.data() + pos, sizeof(int));
+        pos += sizeof(int);
+        m.observaciones = data.substr(pos, len_obs);
+        return m;
     }
 };
 
@@ -47,18 +95,18 @@ public:
         metaFilename = metaFile;
     }
 
-    vector<Record> load() {
+    vector<Matricula> load() {
         // TODO: devolver todos los registros activos
         return {};
     }
 
-    void add(const Record& record) {
+    void add(const Matricula& record) {
         // TODO: agregar un registro O(1)
     }
 
-    Record readRecord(int pos) {
+    Matricula readRecord(int pos) {
         // TODO: leer registro en posición pos
-        return Record();
+        return Matricula();
     }
 
     void remove(int pos) {
